699916d16160e8d872edc14aea8a9b63af4ab299
Rizwankmc <rizwanssv@gmail.com>
2021-10-12 22:53:05 +0530

    server

--- server/server.js
+++ server/server.js
@@ -7,12 +7,20 @@ import socketConnection from "./socketConnection/socketConnection.js";
 const app = express();
 app.use(cors());
 
+const port = process.env.PORT || 3001;
+
 // initiating the instance of server
 const httpServer = createServer(app);
 const io = new Server(httpServer, {});
 
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Rizwan Tic tac toe Server</h1><br /><p>Server is running</p>
+  `);
+});
+
 // socket function to deduct user connection to the user
 socketConnection(io);
-httpServer.listen(3001, () => {
-  console.log("Server is Running at port 3001");
+httpServer.listen(port, () => {
+  console.log(`Server is Running at port ${port}`);
 });
